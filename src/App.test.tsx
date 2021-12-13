275f6343a9213570a22ee6990351b1ae21480a14
corwic <v.corwic@gmail.com>
2021-12-13 01:13:03 +0100

    Get is tested

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, fireEvent, waitFor, screen} from '@testing-library/react'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Integration tests of REST API', () => {
+
+  test('loads and display movie', async () => {
+      render(<App/>)
+      await waitFor(() => screen.getByText('Accept'))
+      
+      expect(screen.getByText('Star Wars: Episode VII - The Force Awakens'))
+  })
+
+  test('after a click', async () => {
+      render(<App/>)
+      await waitFor(() => screen.getByText('Accept'))
+      fireEvent.click(screen.getByText('Accept'))
+
+      expect(screen.getByText('Clifford'))
+  })
+
+  test('after the last movie', async () => {
+      render(<App/>)
+      await waitFor(() => screen.getByText('Accept'))
+      fireEvent.click(screen.getByText('Accept'))
+      fireEvent.click(screen.getByText('Accept'))
+      fireEvent.click(screen.getByText('Accept'))
+      fireEvent.click(screen.getByText('Accept'))
+      fireEvent.click(screen.getByText('Accept'))
+      fireEvent.click(screen.getByText('Accept'))
+
+      expect(screen.getByText('No more movies in your list'))
+  })
+})
